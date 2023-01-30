e9fa915cf4926660a7242af9c199323bcef9d1d3
Dante Roldan <danteroldan72@gmail.com>
2023-01-30 15:10:49 -0300

    upload

--- Imagenes/src/upload.php
+++ Imagenes/src/upload.php
@@ -7,8 +7,16 @@ $tmpFile = $_FILES['file']['tmp_name'];
 
 $manager = new ImageManager(['driver' => 'imagick']);
 
-$img = $manager→make($tmpFile);
 
-$img→save(__DIR__ . '/img/original/' . $fileName);
+$img = $manager -> make($tmpFile);
+
+$img -> save(__DIR__ . '/img/original/' . $fileName);
+
+$img->resize(300, 300, function ($constraint) {
+    $constraint->aspectRatio();
+    $constraint->upsize();
+});
+
+$img->save(__DIR__ . '/img/thumbnails/' . $fileName);
 
 ?>
