4ee6c24b5d7cb58582fb55ad25afe3dfa293fb61
Dante Roldan <danteroldan72@gmail.com>
2023-02-02 17:45:24 -0300

    maquetado final

--- Markdown/src/home.php
+++ Markdown/src/home.php
@@ -9,18 +9,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="src/resources/main.css">
     <title>Inicio</title>
 </head>
 <body>
+<?php
 
-<h1>Bienvenido a mi blog</h1>
+    require 'resources/nav.php';
+    ?>
+
+<div class="post-container">
+    <h1>Bienvenido a mi blog</h1>
+
     <?php
     
     $posts = Post::getPosts();
 
     foreach($posts as $post){
-     echo "<div><a href='{$post->getUrl()}'> {$post->getPostName()} </a></div>";
+     echo "<div><a href='{$post->getUrl()}' class='item-post' > {$post->getPostName()} </a></div>";
     }
     ?>
+</div>
 </body>
 </html>
--- Markdown/src/post/index.php
+++ Markdown/src/post/index.php
@@ -14,16 +14,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="src/resources/main.css">
     <title>Blog</title>
 </head>
 <body>
-    <?php
 
-    
-        echo $post->getContent();
-    
-       
-      
+    <?php
+    require 'src/resources/nav.php';
     ?>
+    <div class="post-container">
+
+        <?php
+
+echo $post->getContent();
+
+?>
+</div>
 </body>
 </html>
