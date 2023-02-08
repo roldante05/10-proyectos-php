4e1a906dc8a8ff5daa8793f9f7f8e97eedabb892
Dante Roldan <danteroldan72@gmail.com>
2023-02-08 14:42:07 -0300

    cambios

--- App-Gastos/schema.sql
+++ App-Gastos/schema.sql
@@ -1,15 +1,14 @@
 CREATE TABLE categories (
-    id int NOT NULL AUTO_INCREMENT,
-    name varchar(255) NOT NULL,
-    PRIMARY KEY (id)
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name varchar(255) NOT NULL
+     
 );
 
 CREATE TABLE expenses (
-    id NOT NULL AUTO_INCREMENT,
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title varchar(255) NOT NULL,
     category_id int NOT NULL,
     expense float(5,2) NOT NULL,
     date date NOT NULL,
-    PRIMARY KEY (id)
-    FOREIGN KEY (category_id) REFERENCES categories(id)
+    FOREIGN KEY (category_id) REFERENCES categories (id)
 );
