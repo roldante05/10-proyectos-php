4446a3ef6377e8f552d5733d03e41ca37af1f9a2
Dante Roldan <danteroldan72@gmail.com>
2023-02-08 17:45:52 -0300

    maquetado nav

--- Gastos-web/src/home.php
+++ Gastos-web/src/home.php
@@ -6,23 +6,21 @@ $expenses = Expense::getAll();
 $total = Expense::getTotal($expenses);
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>Inicio</title>
     <link rel="stylesheet" href="src/resources/main.css">
 </head>
 
 <body>
 <div class="container-nav purple topBotomBordersIn">
-  <a>HOME</a>
-  <a>ARTICLES</a>
-  <a>PORTFOLIO</a>
-  <a>ABOUT</a>
-  <a>CONTACT</a>
+  <a href="?view=home">INICIO</a>
+  <a href="?view=crear-categoria" >CREAR CATEGORIA</a>
+  <a href="?view=crear-expensa" >CREAR EXPENSA</a>
 </div>
     <div class="container">
 
