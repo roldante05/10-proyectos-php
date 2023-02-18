5e7c8ecf53f21ea34d89d79a2824cd00294bdc55
Dante Roldan <danteroldan72@gmail.com>
2023-02-18 19:17:17 -0300

    cambio de routeo

--- Login/src/app.php
+++ Login/src/app.php
@@ -1,12 +1,16 @@
 <?php 
 
 if(isset($_GET['view'])){
- $view = $_GET['view'];
 
+    $view = $_GET['view'];
  require 'src/'. $view . '.php';
+
+} else if(isset($_GET['form'])){
+
+    $view = $_GET['form'];
+    require 'src/form/'. $view . '.php';
+    
 } else {
     
  require 'src/bienvenido.php';
 }
-
-?>
