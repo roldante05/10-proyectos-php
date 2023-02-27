e8169cdf7aa09cfde0df567185cc323cdca79b86
Dante Roldan <danteroldan72@gmail.com>
2023-02-27 20:28:22 -0300

    nav

---
+++ Login/src/nav.php
@@ -0,0 +1,8 @@
+<nav>
+  <ul>
+    <li><a href="?view=inicio">Inicio</a></li>
+    <li><a href="?view=nosotros">Acerca de</a></li>
+    <li><a href="?view=contacto">Contacto</a></li>
+    <li class="login">  <a href="?form=cerrar-session" class="">Cerrar Sesión </a></li>
+  </ul>
+</nav>
